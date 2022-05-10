4ae1ad85402a452bef6767826f441a817ee004b9
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-05-10 12:11:18 +0100

    passing test for two terms

--- lib/evaluate_terms.rb
+++ lib/evaluate_terms.rb
@@ -1,10 +1,13 @@
 class EvaluateTerms 
 
   def evaluate(terms)
-    
-
-
-    [terms, terms.to_f]
+    components = terms.split(" ")
+    if components.length == 1
+      [terms, terms.to_f]
+    else
+      total = components[0].to_f + components[2].to_f
+      [terms, total]
+    end
   end
 
 end
