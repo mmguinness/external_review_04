eef3aa833706ecb86e00addeea2d667c13e4870e
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-05-10 11:30:43 +0100

    First test passing without hardcoded float.

--- lib/evaluate_terms.rb
+++ lib/evaluate_terms.rb
@@ -1,7 +1,7 @@
 class EvaluateTerms 
 
   def evaluate(terms)
-    [terms, 2.0]
+    [terms, terms.to_f]
   end
 
 end
--- spec/evaluate_terms_spec.rb
+++ spec/evaluate_terms_spec.rb
@@ -5,7 +5,6 @@ describe EvaluateTerms do
   describe '#evaluate' do
     it 'returns a single term with the original term and evaluated total' do
       evaluate_terms = EvaluateTerms.new
-
       expect(evaluate_terms.evaluate("2")).to eq ["2", 2.0]
     end
   end
