64d6c7d4d697c11550717e8378d361afd8ab2d9d
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-05-10 12:18:00 +0100

    Refactor of naming and tests

--- lib/evaluate_terms.rb
+++ lib/evaluate_terms.rb
@@ -1,11 +1,14 @@
 class EvaluateTerms 
+  def initialize
+    @components = []
+  end
 
-  def evaluate(terms)
-    components = terms.split(" ")
-    if components.length == 1
+  def evaluate_sum(terms)
+    @components = terms.split(" ")
+    if @components.length == 1
       [terms, terms.to_f]
     else
-      total = components[0].to_f + components[2].to_f
+      total = @components[0].to_f + @components[2].to_f
       [terms, total]
     end
   end
--- spec/evaluate_terms_spec.rb
+++ spec/evaluate_terms_spec.rb
@@ -2,25 +2,22 @@ require './lib/evaluate_terms.rb'
 
 describe EvaluateTerms do
 
-  describe '#evaluate' do
+  subject(:evaluate_terms) { described_class.new}
+  describe '#evaluate_sum' do
     it 'when passed a single term, returns the original term and evaluated total' do
-      evaluate_terms = EvaluateTerms.new
-      expect(evaluate_terms.evaluate("2")).to eq ["2", 2.0]
+      expect(evaluate_terms.evaluate_sum("2")).to eq ["2", 2.0]
     end
 
     it 'when passed a single negative term, returns the original term and evaluated total' do
-      evaluate_terms = EvaluateTerms.new
-      expect(evaluate_terms.evaluate("-2")).to eq ["-2", -2.0]
+      expect(evaluate_terms.evaluate_sum("-2")).to eq ["-2", -2.0]
     end
 
     it 'when passed two terms, returns the original terms and evaluated total' do
-      evaluate_terms = EvaluateTerms.new
-      expect(evaluate_terms.evaluate("2 + 3")).to eq ["2 + 3", 5.0]
+      expect(evaluate_terms.evaluate_sum("2 + 3")).to eq ["2 + 3", 5.0]
     end
 
     it 'when passed two terms, returns the original terms and evaluated total' do
-      evaluate_terms = EvaluateTerms.new
-      expect(evaluate_terms.evaluate("-2 + 3")).to eq ["-2 + 3", 1.0]
+      expect(evaluate_terms.evaluate_sum("-2 + 3")).to eq ["-2 + 3", 1.0]
     end
 
   end
